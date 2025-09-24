6421a11bfdd8a36e657e3c78934efa6a04c8e2db
webwizard1433 <webwizard143@gmail.com>
2025-09-25 01:39:36 +0530

    Release: Version 1.0.1

--- electron-main.js
+++ electron-main.js
@@ -1,6 +1,11 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, dialog } = require('electron');
 const path = require('path');
 const { autoUpdater } = require('electron-updater');
+const log = require('electron-log');
+
+// --- Configure Logging ---
+autoUpdater.logger = log;
+autoUpdater.logger.transports.file.level = 'info';
 
 // IMPORTANT: Replace this with your actual Render application URL
 const RENDER_URL = 'https://exam-prep-hub.onrender.com';
@@ -10,13 +15,15 @@ function createWindow() {
     const mainWindow = new BrowserWindow({
         width: 1280,
         height: 800,
+        title: 'Exam Prep Hub', // Set the initial window title
         webPreferences: {
             // It's more secure to keep nodeIntegration off when loading remote content
             nodeIntegration: false,
             contextIsolation: true,
         },
         // Optional: Set an icon for your application window
-        // icon: path.join(__dirname, 'build/icon.png') 
+        // The icon is set by electron-builder, but this is good for development
+        icon: path.join(__dirname, 'build/icon.ico') 
     });
 
     // Load the live application from Render
@@ -29,9 +36,40 @@ function createWindow() {
 // This method will be called when Electron has finished initialization
 app.whenReady().then(() => {
     createWindow();
+    log.info('App starting...');
 
-    // Check for updates and notify the user
-    autoUpdater.checkForUpdatesAndNotify();
+    // --- Auto-Updater Logic ---
+    // We replace checkForUpdatesAndNotify() with a manual check to handle events.
+    autoUpdater.checkForUpdates();
+
+    autoUpdater.on('update-available', (info) => {
+        log.info('Update available.', info);
+    });
+
+    autoUpdater.on('update-not-available', (info) => {
+        log.info('Update not available.', info);
+    });
+
+    autoUpdater.on('error', (err) => {
+        log.error('Error in auto-updater. ' + err);
+    });
+
+    autoUpdater.on('download-progress', (progressObj) => {
+        let log_message = "Download speed: " + progressObj.bytesPerSecond;
+        log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
+        log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
+        log.info(log_message);
+    });
+
+    autoUpdater.on('update-downloaded', (info) => {
+        log.info('Update downloaded.', info);
+        // The update will be installed silently on the next app quit.
+        // You can also prompt the user to restart.
+        dialog.showMessageBox({
+            title: 'Update Ready',
+            message: 'A new version of Exam Prep Hub has been downloaded. It will be installed on the next restart.'
+        });
+    });
 
     app.on('activate', function () {
         // On macOS it's common to re-create a window in the app when the
